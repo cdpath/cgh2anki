e89efba992f24d8a6473c7b86a8d3589c6b0a52e
cdpath <cdpath@outlook.com>
2016-06-01 02:52:24 +0800

    add logger

--- README.md
+++ README.md
@@ -1,14 +1,12 @@
-# cgh2anki
-
-[重光会](http://blog.sina.com.cn/chungkwongwui)
-
 usage
 
 ```bash
 pip3 install -r scripts/requirements.txt
 # download blogs
-python3 scripts/get_data.py to_read data/cgh_blogs.txt
+python3 scripts/get_data.py to_read data/blog.txt
 # convert to csv
-python3 scripts/get_data.py for_anki data/cgh_blogs.txt data/cgh_anki.csv
+python3 scripts/get_data.py for_anki data/blog.txt data/blog.csv
 ```
+
+[重光会](http://blog.sina.com.cn/chungkwongwui) &copy; 2015 - 2016
 
--- scripts/get_data.py
+++ scripts/get_data.py
@@ -1,24 +1,18 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-__author__ = 'cdpath'
-
 from urllib.request import urlopen, build_opener
 from urllib.parse import urlencode
 from bs4 import BeautifulSoup
 from manager import Manager
-manage_app = Manager()
 import json
 import csv
-import sys
-import re
 
 
+manage_app = Manager()
 pages = set()
-pat = re.compile(r'时代の句变.*\d{6}')
-baseurl = 'http://blog.sina.cn/dpool/blog/newblog/riaapi/mblog/get_articlelist.php'
 
 
-def get_articlelist(baseurl):
+def get_blog_list(baseurl):
     global pages
     params = {'uid': 5753173330, 'pagesize': 100, 'page': 1, 'class_id': -1}
     params = urlencode(params)
@@ -26,18 +20,17 @@ def get_articlelist(baseurl):
         resp = urlopen(baseurl, params.encode('ascii'))
         resp_json = json.loads(resp.read().decode('utf-8'))
         articlelist = resp_json['data']['msg']
-
         for article in articlelist:
             pages.add(article.get('url'))
     except Exception as e:
         print(e)
 
 
-def parse_content(Url):
-#    html = urlopen(Url)
+def parse_content(blog_url):
+    # html = urlopen(Url)
     opener = build_opener()
     opener.addheaders = [('User-agent', 'Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B143 Safari/601.1')]
-    html = opener.open(Url)
+    html = opener.open(blog_url)
 
     bsObj = BeautifulSoup(html, "html.parser")
     contents = bsObj.findAll("div", {"class": "content b-txt1"}) + bsObj.findAll("div", {"class": "item_hide"})
@@ -48,70 +41,116 @@ def replace_with_newlines(element):
     text = ''
     for elem in element.recursiveChildGenerator():
         if isinstance(elem, str):
-            text += elem.replace('\n', '')
+            text += elem.replace('\n', ' ')
         elif elem.name == 'br':
             text += '\n'
     return text
 
 
+def normalize_punctuation(text):
+    import unicodedata
+    text = unicodedata.normalize('NFKC', text)
+
+    import re
+    tr = {'【' : '[', '】':']', '〔' : '[', '〕' : ']', '〈' : '(', '〉' : ')', '“' : '「', '”' : '」', '‘' : '「', '’' : '」', '⋯': '...'}
+    pat = re.compile(u'|'.join(tr.keys()))
+    text = pat.sub(lambda x: tr[x.group()], text)
+    return text
+
+
 def get_csv(input_file, output_file):
-    def __records(input_file):
-        record = list()
-        with open(input_file) as f:
+    def __records(input_f):
+        import re
+        pat = re.compile(r'^.*时代の句变.*\d{6}')
+        segment = list()
+        with open(input_f) as f:
             while True:
                 line = f.readline()
                 if line:
                     if pat.match(line):
-                        yield record
-                        record = list()
-                    record.append(line.strip('\n'))
+                        yield segment
+                        segment = list()
+                    segment.append(line.strip('\n'))
                 else:
                     break
 
+    def __select_by(keyword):
+        import re
+        pat = re.compile(r".*(?=%s)" % keyword.replace('[', '\['))
+        hits = [pat.sub('', i) for i in record if keyword in i]
+        return '<br>'.join(hits)
+
     with open(output_file, 'wt') as f:
         f_csv = csv.writer(f)
+        f_csv.writerow(('时', '事', '译', '辞', '义', '例', '评'))
         for record in __records(input_file):
             if len(record) > 1:
-                t_date = ';'.join(filter(lambda i: i.startswith('时代'), record))
-                t_news = ';'.join(filter(lambda i: i.startswith('【事】'), record))
-                t_news_zh = ';'.join(filter(lambda i: i.startswith('［译］'), record))
-                t_entry = ';'.join(filter(lambda i: i.startswith(('【辞】','［辞］')), record))
-                t_definition = ';'.join(filter(lambda i: i.startswith(('【义】', '［义］')), record))
-                t_example = ';'.join(filter(lambda i: i.startswith(('【例】', '［例］')), record))
-                t_note = ';'.join(filter(lambda i: i.startswith('［评］'), record))
+                t_date = __select_by('时代の句变')
+                t_news = __select_by('[事]')
+                # fixme
+                t_news_zh = '[' + __select_by('译]')
+                t_entry = __select_by('[辞]')
+                t_definition = __select_by('[义]')
+                t_example = __select_by('[例]')
+                t_note = __select_by('[评]')
 
                 f_csv.writerow((t_date, t_news, t_news_zh, t_entry, t_definition, t_example, t_note, ))
 
 
 @manage_app.command
-def to_read(output):
+def to_read(output_file):
     """ download blogs """
-    get_articlelist(baseurl)
-    print("# Go", file=sys.stderr)
+    baseurl = 'http://blog.sina.cn/dpool/blog/newblog/riaapi/mblog/get_articlelist.php'
+    get_blog_list(baseurl)
+    logger.info("=== Start  ===")
 
-    with open(output, 'wt') as f:
+    with open(output_file, 'wt') as f:
         for url in pages:
             contents = parse_content(url)
-            print(url, file=sys.stderr)
+            logger.info("blog: %s" % url)
             for i in contents:
                 try:
                     i = replace_with_newlines(i)
+                    i = normalize_punctuation(i)
                     i = i.strip()
                 except TypeError as e:
-                    print(e)
+                    logger.error('Error Found: %s' % e)
                 else:
                     f.write(i)
 
-    print("# Done", file=sys.stderr)
+    logger.info("=== End ===")
 
 
 @manage_app.command
-def for_anki(input, output):
+def for_anki(input_file, output_file):
     """ convert to CSV to import to Anki """
-    # :%s/时代の句变/\r时代の句变/g
-    get_csv(input, output)
+    get_csv(input_file, output_file)
 
+
+@manage_app.command
+def normalize(input_file, output_file):
+    with open(input_file, 'rt') as in_f, open(output_file, 'wt') as ou_f:
+        while True:
+            line = in_f.readline()
+            if line:
+                line = normalize_punctuation(line)
+                line = line.strip()
+                print(line, file=ou_f)
+            else:
+                break
 
 if __name__ == '__main__':
+    import logging
+    logging.basicConfig(
+        filename='anki.log',
+        filemode='w',
+        level=logging.INFO,
+        format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',
+    )
+    logger = logging.getLogger('anki')
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.ERROR)
+    logger.addHandler(ch)
+
     manage_app.main()
 
